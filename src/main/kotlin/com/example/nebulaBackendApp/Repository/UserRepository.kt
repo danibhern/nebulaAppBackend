9189a0d306b6a67b7a78f273f3679b30efa61e5e
danibhern <dan.barrerah@duocuc.cl>
2025-11-25 22:56:27 -0300

    MAS ARREGLOS

--- src/main/kotlin/com/example/nebulaBackendApp/Repository/UserRepository.kt
+++ src/main/kotlin/com/example/nebulaBackendApp/Repository/UserRepository.kt
@@ -1,6 +1,6 @@
 package com.example.nebulaBackendApp.Repository
 
-import com.example.nebulaBackendApp.model.User
+import com.example.nebulaBackendApp.Model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.Optional
