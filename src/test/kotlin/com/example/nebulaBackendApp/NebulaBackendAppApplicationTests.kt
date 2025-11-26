56b424a183e706e7af3e2295bbd106c95a29b194
danibhern <dan.barrerah@duocuc.cl>
2025-11-26 01:15:26 -0300

    arreglos

--- src/test/kotlin/com/example/nebulaBackendApp/NebulaBackendAppApplicationTests.kt
+++ src/test/kotlin/com/example/nebulaBackendApp/NebulaBackendAppApplicationTests.kt
@@ -2,12 +2,11 @@ package com.example.nebulaBackendApp
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NebulaBackendAppApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {}
 }
